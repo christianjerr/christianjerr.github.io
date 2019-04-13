9563539db5ddf2e8eb6fcdff95e9b476a26cca2a
Christian Jerr T. Buarao <44043066+christianjerr@users.noreply.github.com>
2019-04-13 19:18:51 +0800

    Add files via upload

---
+++ precache-manifest.e162e622943be615f9d8d95cc8aa50d8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "74040eb522498327ed228a42d09d663c",
+    "url": "/static/media/access.74040eb5.png"
+  },
+  {
+    "revision": "5c629d572b1d0b39512b",
+    "url": "/static/css/main.4b7310eb.chunk.css"
+  },
+  {
+    "revision": "fb1c352bb0feeb367d41",
+    "url": "/static/js/1.fb1c352b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8af302d8d2cd6c6ebb8e0bbcb63ecb97",
+    "url": "/static/media/resume.8af302d8.docx"
+  },
+  {
+    "revision": "35934fe13dc37528fbfe19694de0e639",
+    "url": "/static/media/bakery.35934fe1.png"
+  },
+  {
+    "revision": "b40b15d802110aeff7f2667a1a9fe42d",
+    "url": "/static/media/momotors.b40b15d8.png"
+  },
+  {
+    "revision": "5c629d572b1d0b39512b",
+    "url": "/static/js/main.5c629d57.chunk.js"
+  },
+  {
+    "revision": "6a903f8b3e9532123416aae5ac8bc2b3",
+    "url": "/static/media/market.6a903f8b.png"
+  },
+  {
+    "revision": "8a016f47e62e0c97f72909743339821b",
+    "url": "/static/media/airshoes.8a016f47.png"
+  },
+  {
+    "revision": "ea32f84718210c756e85ee24a70f606b",
+    "url": "/static/media/pet.ea32f847.png"
+  },
+  {
+    "revision": "c0fbf5652cd9cae5598aa6ee59e17782",
+    "url": "/static/media/versett.c0fbf565.png"
+  },
+  {
+    "revision": "09f6b2ba02572f19a7d7dd2b5f6494ad",
+    "url": "/static/media/gothic.09f6b2ba.ttf"
+  },
+  {
+    "revision": "b55f943370841f7d0b710539c2ca2d15",
+    "url": "/static/media/bird.b55f9433.jpg"
+  },
+  {
+    "revision": "9610dde9d6159709d8491971e416bd40",
+    "url": "/index.html"
+  }
+];
